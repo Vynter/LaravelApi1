b8dfe67420840c3b956716fb5d79efaefc58e6fa
Vynter <amine-cheraitia@hotmail.com>
2020-08-22 02:31:32 +0200

    fix navbar auth check

--- blog/resources/views/layouts/component/navbar.blade.php
+++ blog/resources/views/layouts/component/navbar.blade.php
@@ -1,7 +1,10 @@
 
+@if (Auth::check())
+
 <div class="navbar navbar-light bg-light">
 
     <a href="/articles" class="navbar-brand">Article index</a>
     <a href="/articles/create" class="navbar-brand">Article create</a>
 </div>
+@endif
 
